c09c57959a91d7e95e17ff59b733e338d0a1369e
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-15 14:30:29 -0600

    feat: agrega pipe a método get de estados para gestión de resultados

--- src/app/core/services/estado.service.ts
+++ src/app/core/services/estado.service.ts
@@ -1,17 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, take } from 'rxjs';
+import { catchError, Observable, of, take, tap } from 'rxjs';
 import { environment } from 'src/environment/environment';
+import { SnackBarService } from './snack-bar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstadoService {
   private apiUrl = environment.apiUrl;
-  
-  constructor(private httpClient: HttpClient) { }
+
+  constructor(private httpClient: HttpClient, private snackBar: SnackBarService) { }
 
   public getEstados(): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/Estado/obtener-estados`).pipe(take(1));
+    return this.httpClient.get(`${this.apiUrl}/Estado/obtener-estados`).pipe(
+      take(1),
+      tap((data: any) => {
+        this.snackBar.openSnackBar(data.mensaje, 3000)
+        return of(true);
+      }),
+      catchError(error => {
+        this.snackBar.openSnackBar(error.mensaje, 3000)
+        return of(false);
+      })
+    );
   }
 }
