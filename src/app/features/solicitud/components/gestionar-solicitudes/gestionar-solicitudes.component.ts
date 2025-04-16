332068671989d2cc434fd665d4ae6fefd8ee33fe
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-15 18:19:19 -0600

    feat: implementa lógica para insertar y actualizar solicitudes

--- src/app/features/solicitud/components/gestionar-solicitudes/gestionar-solicitudes.component.ts
+++ src/app/features/solicitud/components/gestionar-solicitudes/gestionar-solicitudes.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { filter, Observable, skip, Subscription, take, tap } from 'rxjs';
+import { Estado } from 'src/app/core/models/estado';
+import { Solicitud } from 'src/app/core/models/solicitud.model';
+import { createSolicitud, loadEstados, updateSolicitud } from 'src/app/state/solicitud/redux/actions/solicitud.actions';
+import { selectAllEstados, selectEstadoOperacion } from 'src/app/state/solicitud/redux/solicitud.selectors';
 
 @Component({
   selector: 'app-gestionar-solicitudes',
@@ -6,6 +14,71 @@ import { Component } from '@angular/core';
   templateUrl: './gestionar-solicitudes.component.html',
   styleUrls: ['./gestionar-solicitudes.component.scss']
 })
-export class GestionarSolicitudesComponent {
+export class GestionarSolicitudesComponent implements OnInit {
+  protected estados$: Observable<Estado[]>;
+  protected solicitudID: number = 0;
+  protected solicitudFormGroup: FormGroup = new FormGroup({
+    id: new FormControl(0, Validators.required),
+    fechaSolicitud: new FormControl(null),
+    solicitante: new FormControl(''),
+    estado: new FormControl(null)
+  });
 
+  private suscription: Subscription | null = null;
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.estados$ = this.store.select(selectAllEstados);
+  }
+
+  ngOnInit(): void {
+    this.initSuscription();
+    this.store.dispatch(loadEstados());
+
+    this.estados$.pipe(
+      filter(data => data.length > 0),
+      take(1)
+    ).subscribe(() => this.loadSolicitud());
+  }
+
+  private initSuscription() {
+    this.suscription?.unsubscribe;
+    this.suscription = this.store.select(selectEstadoOperacion)
+      .pipe(skip(1))
+      .subscribe((data) => {
+        if (data) this.router.navigate(['solicitudes']);
+      })
+  }
+
+  private loadSolicitud(): void {
+    this.solicitudID = Number(this.route.snapshot.paramMap.get('id') ?? 0);
+    const solicitud: Solicitud | null = history.state.solicitud ?? null;
+    if (!solicitud) return;
+
+    this.solicitudFormGroup.patchValue({
+      id: solicitud.id,
+      fechaSolicitud: new Date(solicitud.fechaSolicitud ?? ''),
+      solicitante: solicitud.solicitante ?? '',
+      estado: solicitud.idEstado ?? null
+    })
+  }
+
+  protected saveSolicitud() {
+    const solicitud: Solicitud = this.getSolicitud();
+
+    if (this.solicitudID === 0) this.store.dispatch(createSolicitud({ solicitud }))
+    else this.store.dispatch(updateSolicitud({ solicitud }));
+  }
+
+  private getSolicitud(): Solicitud {
+    return {
+      id: Number(this.solicitudFormGroup.controls['id'].value),
+      fechaSolicitud: this.solicitudFormGroup.controls['fechaSolicitud'].value ?? null,
+      solicitante: this.solicitudFormGroup.controls['solicitante'].value ?? null,
+      idEstado: this.solicitudFormGroup.controls['estado'].value ?? null
+    }
+  }
 }
